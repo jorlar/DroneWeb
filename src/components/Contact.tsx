a38fa9a7abd641d73010ea40079fd187d599f936
jorlar <jqern.larsen@gmail.com>
2025-03-15 20:03:37 +0100

    :bug: Temporary remove contact form

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -74,7 +74,7 @@ function Contact() {
       <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12'>
         <div className='grid grid-cols-1 lg:grid-cols-2 gap-12'>
           {/* Contact Form */}
-          <div className='card-industrial p-8'>
+          {/* <div className='card-industrial p-8'>
             {submitted ? (
               <div className='bg-energy-500/20 border-2 border-energy-500 rounded-md p-4 text-industrial-950 font-bold'>
                 Takk for din melding! Vi tar kontakt med deg så fort som mulig.
@@ -174,7 +174,7 @@ function Contact() {
                 </button>
               </form>
             )}
-          </div>
+          </div> */}
 
           {/* Contact Information */}
           <div className='space-y-8'>
